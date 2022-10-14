9afce5ee9120b9bb5c8eba9a1dfc61a07353b275
233759-Nishad <113493480+233759-Nishad@users.noreply.github.com>
2022-10-14 17:54:42 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
 import { Component } from '@angular/core';
 
+
+
+
+
+
+
 @Component({
+
+
+
   selector: 'app-root',
+
+
+
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+
+
+  styleUrls: ['./app.component.css'],
+
+
+
 })
+
+
+
 export class AppComponent {
-  title = 'Angular_main';
+
+
+
+  title = 'first_project';
+
+
+
+  list:any[]=[];
+
+  list2:any[]=[];
+
+
+
+ 
+
+
+
+  EnterSubmit(item:String,bo:any,bi:any)
+
+
+
+    {
+
+
+
+     
+
+
+
+      if(item!="")
+
+
+
+this.list.push({item});
+
+this.list2.push({bi});
+
+
+
+bo.value=""
+
+
+
+
+
+}
+
+
+
+  remove(i:number)
+
+
+
+  {
+
+
+
+this.list.splice(i,1);
+
+
+
+  }
 }
